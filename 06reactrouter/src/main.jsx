17ec5d50d0726687fbb9ed0ff563e189608cfd8b
Rohit kumar singh <83065022+r0hitkumarsingh@users.noreply.github.com>
2024-07-26 15:30:36 +0000

    React Router

---
+++ 06reactrouter/src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+
+      {
+        path: "about",
+        element: <About />
+      },
+      
+      {
+        path: "contact",
+        element: <Contact />
+      },
+
+      {
+        path: "user/:id",
+        element: <User />
+      },
+    ]
+  }
+])
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
