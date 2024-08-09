c3023183e92b324084ce3c18a379c11b0056b862
Rohit kumar singh <83065022+r0hitkumarsingh@users.noreply.github.com>
2024-08-09 15:45:52 +0000

    React Props

---
+++ 12tailwindandprops/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <h1 className="bg-gray-500 text-red-800 text-center font-mono font-extrabold p-4"> Hello </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
