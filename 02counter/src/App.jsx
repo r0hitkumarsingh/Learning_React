6d43d46e889f23131d4947a09206126072776db0
Rohit kumar singh <83065022+r0hitkumarsingh@users.noreply.github.com>
2024-08-10 08:56:25 +0000

    counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,42 +2,35 @@ import { useState } from "react"
 
 function App() {
 
-  let [Counter, setCounter] = useState(0)
+  let [counter, setCounter] = useState(15)
 
   const addValue = () => {
     
-    if (Counter +1 > 20) {
-      alert("Limit Exceed")
-    }else{
-      setCounter(Counter + 1)
-      console.log(Counter);
-    }
+    setCounter(prevCounter => prevCounter +1)
+    setCounter(prevCounter => prevCounter +1)
+    setCounter(prevCounter => prevCounter +1)
+    setCounter(prevCounter => prevCounter +1)
     
   }
 
   const removeValue = () => {
-    if (Counter - 1 < 0) {
-      alert("Value can't be Negetive")
-    }else{
-      setCounter(Counter - 1)
-      console.log(Counter);
-    }
+    setCounter(counter -1)
   }
 
   return (
 
     <>
     <h1>React Counter</h1>
-    <h2>Counter value : {Counter}</h2>
+    <h2>Counter value : {counter}</h2>
 
     <button onClick = {addValue}>
-      Add Value {Counter}
+      Add Value
     </button> 
     
      <br />
 
     <button onClick={removeValue}>
-      Remove value {Counter}
+      Remove value
     </button>
     </>
 
