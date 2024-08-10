e146b646386cf87133f5a2f0dc12b5d537346cdc
Rohit kumar singh <83065022+r0hitkumarsingh@users.noreply.github.com>
2024-08-10 05:01:30 +0000

    React Props

--- 12tailwindandprops/src/App.jsx
+++ 12tailwindandprops/src/App.jsx
@@ -1,14 +1,44 @@
+import React from "react";
+import Card from "./components/Card.jsx";
 
 function App() {
-  
+  const image = (
+    <img
+      className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+      src="https://images.unsplash.com/photo-1654944990404-7cc4ff0fb2d4?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt="Rachel Picture"
+    />
+  );
+
+  const image2 = (
+    <img
+      className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+      src="https://images.unsplash.com/photo-1664924805275-71ea8a45b94f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt="Rachel Picture"
+    />
+  );
+
+  const image3 = (
+    <img
+      className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+      src="https://images.unsplash.com/photo-1655553144218-a12debf65d08?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt="Rachel Picture"
+    />
+  );
 
   return (
     <>
-    <div>
-      <h1 className="bg-gray-500 text-red-800 text-center font-mono font-extrabold p-4"> Hello </h1>
-    </div>
+      <div>
+        <h1 className="bg-gray-500 text-red-800 text-center font-mono font-extrabold p-4">
+          {" "}
+          Hello{" "}
+        </h1>
+
+        <Card value="Samantha" position="SDE-III" proPic={image} />
+        <Card value="Rachel" position="System Designer" proPic={image2} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 12tailwindandprops/src/components/Card.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div>
+      <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 mb-4">
+        {props.proPic}
+
+        <div className="text-center space-y-2 sm:text-left">
+          <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">{props.value}</p>
+            <p className="text-slate-500 font-medium">{props.position}</p>
+          </div>
+          <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Message
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
