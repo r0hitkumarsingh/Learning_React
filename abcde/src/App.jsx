4f6aa67bd18332786ff5eaa692fa6abf96537952
Rohit kumar singh <83065022+r0hitkumarsingh@users.noreply.github.com>
2025-01-08 09:46:35 +0000

    abcd

---
+++ abcde/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+
+
+  return (
+<>
+<Header />
+    <h1 className='bg-red-500 border-white'> Rohit kumar</h1>
+<Footer />
+</>
+  )
+}
+
+export default App
